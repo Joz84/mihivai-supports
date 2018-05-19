bb72503d9fe910445e6de05c224c8d3572cdeaf0
Jonathan Serafini <jonathan.serafini@gmail.com>
2018-05-19 18:52:43 +0200

    crud landing

---
+++ app/controllers/user/landing_pages_controller.rb
@@ -0,0 +1,65 @@
+class User::LandingPagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_users, only: [:index, :edit]
+
+  def index
+    @landing_page = LandingPage.new
+  end
+
+  def show
+    @landing_page = LandingPage.find(params[:id])
+    render :layout => false
+  end
+
+  def create
+    @landing_page = current_user.landing_pages.new(landing_page_params)
+    if @landing_page.save
+      respond_to do |format|
+        format.html { redirect_to user_landing_pages_path }
+        format.js  { flash.now[:alert] = nil}
+      end
+    else
+      respond_to do |format|
+        format.html { render :index }
+        format.js  { flash.now[:alert] = "Désolé la page n'a pas pu être créée"}
+      end
+    end
+  end
+
+  def edit
+    @landing_page = LandingPage.find(params[:id])
+  end
+
+  def update
+    @landing_page = LandingPage.find(params[:id])
+    if @landing_page.update_attributes(landing_page_params)
+      redirect_to user_landing_page_path(@landing_page), notice: 'Porject mis à jour avec succès'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_users
+    @users = User.where(promotion: current_user.promotion)
+  end
+
+  def landing_page_params
+    params.require(:landing_page).permit(
+      :title,
+      :html,
+      :css,
+      landing_pages_users_attributes: [
+        :id,
+        :user_id,
+        :_destroy
+      ]
+    )
+  end
+end
+
+
--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -6,4 +6,26 @@ class LandingPage < ApplicationRecord
   accepts_nested_attributes_for :landing_pages_users,
                                 reject_if: proc { |attributes| attributes[:user_id].blank? },
                                 allow_destroy: true
+
+  def display(attrs)
+    split_html = html.split("</head>")
+    head = split_html[0]
+    body = split_html[1..-1].join
+    new_html = "#{head}<style>#{css}</style></head>#{body}"
+    if attrs[:with_back_button]
+      split_html = new_html.split("</body>")
+      head = split_html[0]
+      body = split_html[1..-1].join
+      back_button = '<div style="text-align: center; font-family: arial; padding: 23px 0; position: fixed; bottom: 0px; left: 0px; width: 100%; border-top: 1px solid grey; background-color: white;" >
+                      <a href="/" style="text-decoration: none; color: #469AE0; font-size: 20px; font-weight: bold;">Revenir sur MihiVai</a>
+                    </div>
+                    <div style="height: 70px;"></div>'
+      new_html = "#{head}#{back_button}</body>#{body}"
+    end
+    new_html
+  end
+
+  def text
+    ''
+  end
 end
