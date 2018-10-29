b6cb83d1012eec6e8d305b9a24f0625ee5a7aec7
Claudine Pradal <claudinecartailler@gmail.com>
2018-10-29 18:16:14 +0100

    add conditions validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_093658) do
+ActiveRecord::Schema.define(version: 2018_10_29_165307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 2018_06_08_093658) do
     t.string "site"
     t.string "linkedin"
     t.boolean "admin"
+    t.boolean "conditions_validation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["promotion_id"], name: "index_users_on_promotion_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
