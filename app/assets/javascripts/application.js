6e3ed52a20e9a0ad814a8955a9bae4894e090d70
Claudine <clo.pradal@gmail.com>
2019-07-22 16:57:28 +0200

    front integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,14 @@
 //= require jquery_ujs
 //= require cocoon
 //= require_tree .
+ $(window).scroll(function(){
+
+      var wintop = $(this).scrollTop();
+
+    $('#animated_counter').each(function(){
+    if(wintop > $(this).offset().top - ($(window).height())){
+      $(this).addClass('radial-chart__value');
+
+    }
+  });
+});
